3e1c61bd367fc64e82019e4bf5ffed6d67ef5df3
EGWU KINGSLEY OGBONNIA <egwukingsley655@gmail.com>
2022-07-06 02:32:15 -0700

    hard thing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
-* main - Print _putchar
+* main - Entry point
 *
-* Return: Always 0(Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 
 	return (0);
