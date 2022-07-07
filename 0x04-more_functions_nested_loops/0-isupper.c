f415d8f4785ae002956486755b5dd349eea3b51d
EGWU KINGSLEY OGBONNIA <egwukingsley655@gmail.com>
2022-07-07 16:05:18 -0700

    complete

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
-* _isupper - check for uppercase
+* _isupper - Checks for uppercase characters.
+* @c: The character to be checked.
 *
-* @c: character
-*
-* Return: 1 if it is uppercase
+* Return: 1 if character is uppercase, 0 otherwise.
 */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
